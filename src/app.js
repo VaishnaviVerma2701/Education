0a9cc497149fc28d433843f1453c9edb9b3aac58
VaishnaviVerma2701 <vaishnaviverma2701@gmail.com>
2024-02-28 13:37:30 +0530

    source code

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ const port = 4000;
 // require("./db/conn.js");
 
 
-const static_path =path.join(__dirname, "../public");
+const static_path =path.join(__dirname, "../");
 app.use(express.static(static_path));
 
 // Define the schema
